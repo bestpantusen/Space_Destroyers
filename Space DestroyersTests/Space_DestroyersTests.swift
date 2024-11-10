983860656cc0155fc4438411b23c9c3de8750399
bestpantusen <kpantuse@andrew.cmu.edu>
2024-11-10 17:18:47 -0500

    Initial Commit

---
+++ Space DestroyersTests/Space_DestroyersTests.swift
@@ -0,0 +1,17 @@
+//
+//  Space_DestroyersTests.swift
+//  Space DestroyersTests
+//
+//  Created by Kachatad Pantusen on 10/11/2567 BE.
+//
+
+import Testing
+@testable import Space_Destroyers
+
+struct Space_DestroyersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
